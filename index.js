183bdc4cf3ccf6a37e8cbe548e1edd4d78ca8a20
Yesim nur Akar <yesimnurakar@gmail.com>
2021-06-20 14:50:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,33 +1,51 @@
-
 class Polygon {
-  constructor(sides) {
-    this.sides = sides;
-  }
-  get countSides() {
-      return this.sides.length;
+    constructor(sides) {
+        this.sides = sides
+        this.count = this.sides.length
     }
 
-    get perimeter() {
-  let peri = 0;
-    for (let length of this.sides) {
-      peri += length;
+    get countSides() {
+        return this.sides.length
+      }
+
+      get perimeter() {
+        if (!Array.isArray(this.sides)) return;
+        let sum = 0;
+        for (var int of this.sides) {
+          sum += int
+        }
+        return sum
+      }
+
+  }
+
+  class Triangle extends Polygon {
+
+    get isValid() {
+      if (!Array.isArray(this.sides)) return;
+      if (this.count !== 3) return;
+      let side1 = this.sides[0]
+      let side2 = this.sides[1]
+      let side3 = this.sides[2]
+      return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
     }
-    return peri;
-  }
-}
-
-    class Triangle extends Polygon {
-  constructor(countSides, perimeter) {
-    super(countSides);
-      super(perimeter);
   }
 
-get isValid() {
-  if (this.count !== 3) return;
-let side1 = this.sides[0];
-let side2 = this.sides[1];
-let side3 = this.sides[2];
-return((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1));
- }
+  class Square extends Polygon {
 
-}
+    get isValid() {
+      if (!Array.isArray(this.sides)) return;
+      if (this.count !== 4) return;
+      let side1 = this.sides[0]
+      let side2 = this.sides[1]
+      let side3 = this.sides[2]
+      let side4 = this.sides[3]
+      return ((side1 === side2) && (side1 === side3) && (side1 === side4))
+    }
+
+    get area() {
+      if (!Array.isArray(this.sides)) return;
+      if (this.count !== 4) return;
+      return this.sides[0] * this.sides[0]
+    }
+  }
