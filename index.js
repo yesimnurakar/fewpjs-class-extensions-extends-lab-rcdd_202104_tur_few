6d0b38f1d9afe63b3d5dab4aba85f804c85c11e1
Yesim nur Akar <yesimnurakar@gmail.com>
2021-06-20 14:49:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+  get countSides() {
+      return this.sides.length;
+    }
+
+    get perimeter() {
+  let peri = 0;
+    for (let length of this.sides) {
+      peri += length;
+    }
+    return peri;
+  }
+}
+
+    class Triangle extends Polygon {
+  constructor(countSides, perimeter) {
+    super(countSides);
+      super(perimeter);
+  }
+
+get isValid() {
+  if (this.count !== 3) return;
+let side1 = this.sides[0];
+let side2 = this.sides[1];
+let side3 = this.sides[2];
+return((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1));
+ }
+
+}
